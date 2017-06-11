f348e4f35cea0483806a0ca36e0d57bb24699e98
linhng15 <nguyett@lakeforest.edu>
2017-06-11 15:55:48 -0500

    handlebars

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -39,14 +39,18 @@ router.get("/scrape", function(req, res) {
 
   	// Load the body of the HTML into cheerio
    	var $ = cheerio.load(html);
-    // Empty array to save our scraped data
-    var result = [];
-
+    
+    var entries = [];
   	// With cheerio, find each h2-tag with the class "story-heading"
   	$("article.story.theme-summary").each(function(i, element) {
-      
+      debugger
+      console.log(element)
+      // Empty array to save our scraped data
+      var result = {};
+
       // Save the text of the h2-tag as "title"
-      result.title = $("h2.story-heading").text();
+      result.title = $("h2.story-heading >a").text();
+      // console.log(result.title)
 
       // // Find the h2 tag's children a-tag, and save it's href value as "link"
       result.link = $("h2.story-heading").children().attr("href");
@@ -58,6 +62,9 @@ router.get("/scrape", function(req, res) {
       // This effectively passes the result object to the entry (and the title and link)
       var entry = new Article(result);
 
+
+      entries.push(entry)
+
       // Now, save that entry to the db
       entry.save(function(err, doc) {
         // Log any errors
@@ -66,14 +73,16 @@ router.get("/scrape", function(req, res) {
         }
         // Or log the doc
         else {
-          console.log(doc);
+          // console.log(doc);
+          // console.log("added" + result.title)
         }
       });
     });
     // // Tell the browser that we finished scraping the text
-    // res.send("Scrape Complete");
+    console.log(entries.length)
+    res.json(entries);
+    // res.redirect("index")
     // After the program scans each h2.story-heading, log the result
-    console.log(result);
   });
 
 });
@@ -89,84 +98,73 @@ router.get("/", function(req, res) {
     // Or send the doc to the browser as a json object
     else {
       var articleList = {
-        Article: doc
+        articles: doc
+      // res.json(doc)
       }
-      // res.json(doc);
-    }
+    };
     res.render("index", articleList);
+
   });
 });
 
 // Grab an article by it's ObjectId
-// router.get("/articles/:id", function(req, res) {
-//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//   Article.findOne({ "_id": req.params.id })
-//   // ..and populate all of the notes associated with it
-//   .populate("note")
-//   // now, execute our query
-//   .exec(function(error, doc) {
-//     // Log any errors
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Otherwise, send the doc to the browser as a json object
-//     else {
-//       res.json(doc);
-//     }
-//   });
-// });
+router.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  Article.findOne({ "_id": req.params.id })
+  // ..and populate all of the notes associated with it
+  .populate("note")
+  // now, execute our query
+  .exec(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise, send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
 
 
 // // Create a new note or replace an existing note
-// router.post("/articles/:id", function(req, res) {
-//   // Create a new note and pass the req.body to the entry
-//   console.log("i'm trying to see this below")
-//    console.log(req.body)
-//   var newNote = new Note(req.body);
+router.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  // console.log("i'm trying to see this below")
+  //  console.log(req.body)
+  var newNote = new Note(req.body);
 
-
-//   // And save the new note the db
-//   newNote.save(function(error, doc) {
-//     // Log any errors
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Otherwise
-//     else {
-//       // Use the article id to find and update it's note
-//       Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
-//       // Execute the above query
-//       .exec(function(err, doc) {
-//         // Log any errors
-//         if (err) {
-//           console.log(err);
-//         }
-//         else {
-//           // Or send the document to the browser
-//           res.send(doc);
-//         }
-//       });
-//     }
-//   });
-// });
+   // And save the new note the db
+  newNote.save(function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update it's note
+      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+      // Execute the above query
+      .exec(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // Or send the document to the browser
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
 
 
       // for (var i = 0; i < result.length; i++) {
       //  result[i]
       // }
-// router.get("/saved", function(req, res) {
-//   // Grab every doc in the Articles array
-//   Article.find({}, function(error, doc) {
-//     // Log any errors
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Or send the doc to the browser as a json object
-//     else {
-//       res.json(doc);
-//     }
-//   });
-//   res.render("saved");
-// });
+router.get("/saved", function(req, res) {
+  res.render("saved");
+});
 
 module.exports = router;
